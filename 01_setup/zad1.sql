ff890c1dec71b77c4016ff0fbf3f45d42eb565f3
natinkakrol <147758793+natinkakrol@users.noreply.github.com>
2025-10-10 00:58:06 +0200

    Update zad1.sql
    Tworzenie db.

--- 01_setup/zad1.sql
+++ 01_setup/zad1.sql
@@ -1,1 +1,15 @@
-Zmiana konfiguracji X.
+-- Database: firma
+
+-- DROP DATABASE IF EXISTS firma;
+
+CREATE DATABASE firma
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_Europe.1252'
+    LC_CTYPE = 'English_Europe.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
