db49a3bf8f384957665ab54fb13eabcf1608d142
Natalia Król <147758793+natinkakrol@users.noreply.github.com>
2025-10-11 21:00:32 +0200

    Update zad5i.sql

--- 01_setup/zad5i.sql
+++ 01_setup/zad5i.sql
@@ -1,1 +1,6 @@
-
+-- Uszereguj pracowników według pensji.i
+SELECT p.id_pracownika, p.imie, p.nazwisko,  pe.kwota AS pensja
+FROM pracownicy p
+JOIN wynagrodzenie w ON p.id_pracownika=w.id_pracownika
+JOIN pensja pe ON w.id_pensji=pe.id_pensji
+ORDER BY pe.kwota; 
