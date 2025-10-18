f2d93a5e9f8ad10e68b82f97c4ecf686e2ff69dd
Natalia Król <147758793+natinkakrol@users.noreply.github.com>
2025-10-18 12:13:30 +0200

    02_zad2

---
+++ 02_cw/zad2.sql
@@ -0,0 +1,14 @@
+-- Database: bdp
+
+-- DROP DATABASE IF EXISTS bdp;
+
+CREATE DATABASE bdp
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_Europe.1252'
+    LC_CTYPE = 'English_Europe.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
